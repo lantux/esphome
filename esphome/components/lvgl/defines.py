b1d97d6b1e460796148791381e249d5981945cff
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-02-04 08:28:17 +1100

    Add on_swipe_up and friends.

--- esphome/components/lvgl/defines.py
+++ esphome/components/lvgl/defines.py
@@ -167,6 +167,10 @@ TYPE_FLEX = "flex"
 TYPE_GRID = "grid"
 TYPE_NONE = "none"
 
+DIRECTIONS = LvConstant(
+    "LV_DIR_", "LEFT", "RIGHT", "BOTTOM", "TOP", typename="lv_dir_t"
+)
+
 LV_FONTS = list(f"montserrat_{s}" for s in range(8, 50, 2)) + [
     "dejavu_16_persian_hebrew",
     "simsun_16_cjk",
@@ -193,6 +197,9 @@ LV_EVENT_MAP = {
 }
 
 LV_EVENT_TRIGGERS = tuple(f"on_{x.lower()}" for x in LV_EVENT_MAP)
+SWIPE_TRIGGERS = tuple(
+    f"on_swipe_{x.lower()}" for x in DIRECTIONS.choices + ("up", "down")
+)
 
 
 LV_ANIM = LvConstant(
@@ -270,9 +277,6 @@ KEYBOARD_MODES = LvConstant(
     "NUMBER",
 )
 ROLLER_MODES = LvConstant("LV_ROLLER_MODE_", "NORMAL", "INFINITE")
-DIRECTIONS = LvConstant(
-    "LV_DIR_", "LEFT", "RIGHT", "BOTTOM", "TOP", typename="lv_dir_t"
-)
 TILE_DIRECTIONS = DIRECTIONS.extend("HOR", "VER", "ALL")
 CHILD_ALIGNMENTS = LvConstant(
     "LV_ALIGN_",
--- esphome/components/lvgl/trigger.py
+++ esphome/components/lvgl/trigger.py
@@ -9,6 +9,7 @@ from .defines import (
     CONF_Y,
     LV_EVENT_MAP,
     LV_EVENT_TRIGGERS,
+    SWIPE_TRIGGERS,
     literal,
 )
 from .lvcode import (
@@ -43,6 +44,23 @@ async def generate_triggers():
                 w.add_flag("LV_OBJ_FLAG_CLICKABLE")
                 event = literal("LV_EVENT_" + LV_EVENT_MAP[event[3:].upper()])
                 await add_trigger(conf, w, event)
+
+            for event, conf in {
+                event: conf
+                for event, conf in w.config.items()
+                if event in SWIPE_TRIGGERS
+            }.items():
+                conf = conf[0]
+                dir = event[9:].upper()
+                dir = {"UP": "TOP", "DOWN": "BOTTOM"}.get(dir, dir)
+                w.clear_flag("LV_OBJ_FLAG_SCROLLABLE")
+                selected = literal(
+                    f"lv_indev_get_gesture_dir(lv_indev_get_act()) == LV_DIR_{dir}"
+                )
+                await add_trigger(
+                    conf, w, literal("LV_EVENT_GESTURE"), is_selected=selected
+                )
+
             for conf in w.config.get(CONF_ON_VALUE, ()):
                 await add_trigger(
                     conf,
@@ -61,13 +79,14 @@ async def generate_triggers():
                 lv.obj_align_to(w.obj, target, align, x, y)
 
 
-async def add_trigger(conf, w, *events):
+async def add_trigger(conf, w, *events, is_selected=None):
+    is_selected = is_selected or w.is_selected()
     tid = conf[CONF_TRIGGER_ID]
     trigger = cg.new_Pvariable(tid)
     args = w.get_args() + [(lv_event_t_ptr, "event")]
     value = w.get_values()
     await automation.build_automation(trigger, args, conf)
     async with LambdaContext(EVENT_ARG, where=tid) as context:
-        with LvConditional(w.is_selected()):
+        with LvConditional(is_selected):
             lv_add(trigger.trigger(*value, literal("event")))
     lv_add(lvgl_static.add_event_cb(w.obj, await context.get_lambda(), *events))
